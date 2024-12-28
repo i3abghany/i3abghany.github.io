bfd0d9f38edeb1d82232ba98f91ecf881709baac
Mahmoud Abumandour <mam47@sfu.ca>
2024-12-27 19:08:01 -0800

    Add dark mode

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ document.querySelectorAll('.toggle-btn').forEach(button => {
         details.style.display = details.style.display === 'block' ? 'none' : 'block';
     });
 });
+
+const toggleButton = document.getElementById('dark-mode-toggle');
+
+if (localStorage.getItem('dark-mode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+    toggleButton.textContent = '☀️';
+}
+
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('dark-mode', 'enabled');
+        toggleButton.textContent = '☀️';
+    } else {
+        localStorage.setItem('dark-mode', 'disabled');
+        toggleButton.textContent = '🌙';
+    }
+});
